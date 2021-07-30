2e0c9c72cd1ea699324981438e2fa864ced5a30f
Ángela Ruiz <ac.comayagua@yahoo.com>
2021-07-30 17:39:47 -0600

    ya tengo el get

--- logic/NonLocalProducts.py
+++ logic/NonLocalProducts.py
@@ -5,6 +5,14 @@ class NonlocalLogic(PybaLogic):
     def __init__(self):
         super().__init__()
 
-    
+   # get
+    def getNonLocalProductById(self, id):
+        database = self.createDatabaseObj()
+        sql = f"SELECT * FROM helioapibd.nonlocalproducts where ID={id};"
+        result = database.executeQuery(sql)
+        if len(result) != 0:
+            return result[0]
+        else:
+            return {}    
 
     
