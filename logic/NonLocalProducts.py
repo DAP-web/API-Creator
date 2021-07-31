a6eac829c855bbd20b65ed15bb7018f13694d444
Ángela Ruiz <ac.comayagua@yahoo.com>
2021-07-30 18:14:51 -0600

    Mejorando el get y post

--- logic/NonLocalProducts.py
+++ logic/NonLocalProducts.py
@@ -6,22 +6,23 @@ class NonlocalLogic(PybaLogic):
         super().__init__()
 
    # get
-    def getNonLocalProductById(self, id):
+    def getNonLocalProduct(self):
         database = self.createDatabaseObj()
-        sql = f"SELECT * FROM helioapibd.nonlocalproducts where ID={id};"
+        sql = f"SELECT * FROM helioapibd.nonlocalproducts where;"
         result = database.executeQuery(sql)
         if len(result) != 0:
             return result[0]
         else:
             return {}
     # post
-    def getNonLocalProductByName(self, Name):
+    def getNonLocalProductByName(self, id):
         database = self.createDatabaseObj()
-        sql = f"SELECT * FROM helioapibd.nonlocalproducts where nombreProducto='{Name}';"
+        sql = f"SELECT * FROM helioapibd.nonlocalproducts where ID={id};"
         result = database.executeQuery(sql)
         if len(result) != 0:
             return result
         else:
             return []    
+
 
     
