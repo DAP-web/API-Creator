464178325987a4f17f63b6e31aed6c96d8235932
marcelamata <65987934+marcelamata@users.noreply.github.com>
2021-07-30 22:43:35 -0600

    Ahora si

--- resources/NonLocal_resources.py
+++ resources/NonLocal_resources.py
@@ -3,31 +3,24 @@ from logic.NonLocalProducts import NonlocalLogic
 
 class NonLocal(Resource):
     def __init__(self):
-        self.city_put_args = self.createParser()
+        self.nonLocal_put_args = self.createParser()
         self.logic = NonlocalLogic()
 
     def createParser(self):
         args = reqparse.RequestParser()
-        args.add_argument("nombreProducto", type=str, help="nombre del producto")
-        args.add_argument("descripcion", type=str, help="descripcion del prodcuto")
         args.add_argument("stock", type=int, help="cantidad de producto")
-        args.add_argument("marca", type=str, help="marca del producto")
-        args.add_argument("urlreference", type=str, help="la url de referencia")
-        args.add_argument("precio", type=int, help="precio del producto")
-        args.add_argument("empresa", type=str, help="la empresa")
+        return args
 
     def get(self, id):
-        result = self.logic.getNonLocalProductById(id)
+        result = self.logic.getNonLocalProduct()
         return result, 200
 
     def post(self, id):
-        nonLocalDict = self.logic.getNonLocalProductById(id)
-        nonLocalCode = nonLocalDict["nombreProducto"]
-        result = self.logic.getNonLocalProductByName(nonLocalCode)
+        result = self.logic.getNonLocalProductByID(id)
         return result, 200
 
     def patch(self, id):
         nonLocal = self.nonLocal_put_args.parse_args()
-        rows = self.logic.updateNonLocalProduct(id,nonLocal)
+        rows = self.logic.updateNonLocalProduct(id, nonLocal)
         return {"rowsAffected": rows}
 
